824a391d3b0555b65b63fc04247d1ba586c1d240
maksymXlol <124809697+maksymXlol@users.noreply.github.com>
2023-07-31 12:51:01 +0100

    hw due 31/07/2023

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // https://api.adviceslip.com/
+
+let btn = document.querySelector(".btn");
+let num = document.querySelector(".advice-number");
+let text = document.querySelector(".advice-text");
+
+btn.addEventListener("click", click);
+
+function click() {
+  console.log(btn);
+}
