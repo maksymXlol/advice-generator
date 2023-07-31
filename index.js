772189a5bc9253584ac53732bf3f7edd9e4adfd0
maksymXlol <124809697+maksymXlol@users.noreply.github.com>
2023-07-31 17:27:40 +0100

    last commit

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ let btn = document.querySelector(".btn");
 let num = document.querySelector(".advice-number");
 let text = document.querySelector(".advice-text");
 
-btn.addEventListener("click", click);
+btn.addEventListener("click", getData);
 
-function click() {
-  console.log(btn);
+async function getData() {
+  let response = await fetch("https://api.adviceslip.com/advice");
+  let data = await response.json();
+  const { id, advice } = data.slip;
+  text.textContent = advice;
+  num.textContent = "advice #" + id;
 }
+
+window.addEventListener("load", getData);
